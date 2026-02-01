d6a0bf4675a808ffe31b2d69bc27536813be2ca3
mnigetha-a11y <mnigetha@gmail.com>
2026-02-01 10:59:19 +0530

    Initial commit with Docker and Gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 
-// Route for root
 app.get("/", (req, res) => {
-  res.send("Farm Management Backend Running");
+  res.send("Farm backend running");
 });
 
-// Listen on port 4000
 app.listen(4000, () => {
   console.log("Server running on port 4000");
 });
